8cc7cd36ed260a230dd358bccdb3f5a775177bf7
Your Name <your_email@whatever.com>
2018-02-10 17:12:05 +0700

    laba4
    slojna

---
+++ Shapes/Shapes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Polymorphism *****\n");
+            // Создать массив совместных с Shape объектов.
+            Shape[] myShapes = { new Hexagon(), new Circle(), new Hexagon("Mick"),
+            new Circle("Beth"), new Hexagon("Linda")};
+
+            // Перейти в цикл по всем элементам и взаимодействовать
+            // с полифорным интерфейсом.
+            foreach (Shape s in myShapes)
+            {
+                s.Draw();
+            }
+            //здесь вызывается метод draw() из класса threedcircle.
+            ThreeDCircle o = new ThreeDCircle();
+            o.Draw();
+            //здесь вызывается метод draw родительского класса!!
+            ((Circle)o).Draw();
+                      
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Shapes/Shapes/ThreeDCircle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    //этот класс расширяет circle и скрывает унаследовнный метод draw().
+    class ThreeDCircle:Circle
+    {
+        // Скрыть поле ShapeName,определенное выше в иерархии
+        protected new string shapeName;
+        //скрыть любую реализацию draw(), находящуюся выше в иерархии
+        public void Draw()
+        {
+            Console.WriteLine("Drawing a 3D Circle");
+        }
+    }
+}
